b37e77ab4dc6f2eb2ed55ceb08158f6ced697831
Radek Obiral <obiralradek@gmail.com>
2023-09-04 16:26:47 +0200

    refactor: make fibonacci.ts more readable

--- src/utils/fibonacci.ts
+++ src/utils/fibonacci.ts
@@ -22,18 +22,18 @@ export function isFibonacciSequence(arr: TileValue[]): boolean {
   return firstDirection || secondDirection;
 }
 
-export function removeAllFiboSequences(
+export function removeAllFiboSequencesFromArray(
   inputArray: TileValue[],
   rowNumber: number,
   vertical = false,
-) {
-  const result = [...inputArray];
+): { newArray: TileValue[]; changes: Location[] } {
+  const newArray = [...inputArray];
   let removedFields: Location[] = [];
 
   for (let start = 0; start + 5 <= inputArray.length; start++) {
     if (isFibonacciSequence(inputArray.slice(start, start + 5))) {
       for (let i = start; i < start + 5; i++) {
-        result[i] = undefined;
+        newArray[i] = undefined;
         removedFields.push({
           y: vertical ? i : rowNumber,
           x: vertical ? rowNumber : i,
@@ -42,22 +42,32 @@ export function removeAllFiboSequences(
     }
   }
 
-  return { newRow: result, changes: removedFields };
+  return { newArray, changes: removedFields };
 }
 
-export function filterOutFiboSequences(board: Board) {
+export function filterOutFiboSequences(board: Board): {
+  filteredBoard: Board;
+  changes: Location[];
+} {
   let newBoardRows: Board = [];
   let newBoardCols: Board = Array(board.length).fill([]);
   let changesArray: Location[] = [];
   board.forEach((row, rowNumber) => {
-    const { newRow, changes } = removeAllFiboSequences(row, rowNumber);
-    newBoardRows.push(newRow);
+    const { newArray, changes } = removeAllFiboSequencesFromArray(
+      row,
+      rowNumber,
+    );
+    newBoardRows.push(newArray);
     changesArray.push(...changes);
   });
   for (let col = 0; col < board[0].length; col++) {
     const column = board.map((row) => row[col]);
-    const { newRow, changes } = removeAllFiboSequences(column, col, true);
-    pushColumn(newBoardCols, newRow);
+    const { newArray, changes } = removeAllFiboSequencesFromArray(
+      column,
+      col,
+      true,
+    );
+    pushColumn(newBoardCols, newArray);
     changesArray.push(...changes);
   }
   for (let row = 0; row < newBoardRows.length; row++) {
